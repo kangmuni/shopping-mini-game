58a6d92209d33ece111543bff9b37a93c88afb54
kangmuni <kangmoonee@gmail.com>
2022-05-26 21:48:04 +0900

    Last Touch

--- src/main.js
+++ src/main.js
@@ -8,8 +8,8 @@ function loadItems() {
 // Update the list with the given items
 function displayItems(items) {
   const container = document.querySelector('.items ul');
-  const html = items.map((item) => createHTMLString(item));
-  console.log(html); // 뭐가 나오는지 궁금할 땐, 이렇게
+  // const html = items.map((item) => createHTMLString(item));
+  // console.log(html);
   // HTML 요소로 각각 아이템을 변환해 줄 것이다. (컨테이너에 innerHTML 업데이트)
   container.innerHTML = items.map((item) => createHTMLString(item)).join('');
 }
@@ -26,8 +26,6 @@ function createHTMLString(item) {
 
 // Handle button click
 function onButtonClick(event, items) {
-  // console.log(event.target.dataset.key);
-  // console.log(event.target.dataset.value);
   // 컨테이너에서 이벤트 리스너를 달았기 때문에 버튼들이 아니여도 컨테이너 안 주위를 누르게 되면 이벤트가 발생하고 있는것도 볼 수 있다.
   // But, undefined로 나오겠지요.
   const dataset = event.target.dataset;
@@ -40,18 +38,7 @@ function onButtonClick(event, items) {
   // const filtered = displayItems(items.filter((item) => item[key] === value));
   // console.log(filtered);
   displayItems(items.filter((item) => item[key] === value));
-  // updateItems(items, key, value);
 }
-
-// Make the items matching {key: value} invisible.
-// function updateItems(items, key, value) {
-//   items.forEach((item) => {
-//     if (item[key] === value) {
-//       item.classList.remove('invisible');
-//     }
-//     item.classList.add('visible');
-//   });
-// }
 
 function setEventListener(items) {
   const logo = document.querySelector('.logo');
@@ -64,12 +51,11 @@ const today = new Date();
 const hours = today.getHours();
 const minutes = today.getMinutes();
 const clock = document.querySelector('.clock');
-clock.innerText = `${hours}:${minutes}`;
+clock.innerText = `${hours}:${minutes < 10 ? `0${minutes}` : minutes}`;
 
 // Main
 loadItems()
   .then((items) => {
-    console.log(items); // Array(33)
     displayItems(items); // 받아온 아이템들을 다시 displayItems 함수로 전달한다.
     setEventListener(items); // 받아온 아이템들을 다시 setEventListener 함수로 전달한다.
   })
